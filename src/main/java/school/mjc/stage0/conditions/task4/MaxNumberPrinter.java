50b73d24a5a8ee5b29f74a5f836c64dacb4bc6ed
avetyan-gayane <avetyan.gayane.01@gmail.com>
2023-07-04 15:00:03 +0400

    task 4_4

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int greatest = 0;
+        if (first >= second && first >= third)
+            greatest = first;
+        else if(second >= first && second >= third)
+            greatest = second;
+        else
+            greatest = third;
+
+        System.out.println(greatest);
     }
 }
